18fc9e239dadd9de5df2f6e0cd35ee3accd3e3c4
charlie <charlie@maddex.dev>
2023-11-07 00:15:28 +0000

    animations and text update

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="canonical" href="https://maddex.dev">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin="true">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;300;400;500;600;700;800;900&amp;display=swap">
@@ -26,17 +27,36 @@
     <meta name="twitter:title" content="Charlie Maddex">
     <meta name="twitter:description" content="Charlie Maddex's Website">
     <meta name="twitter:image" content="https://maddex.dev/favicon.jpg">
-
 </head>
 
-<body>
-    <div id="maincontent" style="margin-top:70px">
-        <h2>Charlie Maddex 🧊</h2>
-        <b>Senior Technical Analyst at Method-IT. Backend Engineer at Multiorb.</b>
-        <br><br>
-        I currently work as a Senior Technical Analyst at a Managed Service Provider (MSP) Method-IT where I focus on architecture and support, prioritizing the security and reliability of the platform through ongoing monitoring, development, patching, and incident response. I am also a Backend Engineer for Multiorb where we are automating optimizations for cloud costs across multiple cloud platforms.
-        <br><br>
-        <small><sup>*</sup> <a href="https://x.com/cunjur/">Twitter</a>, <a href="https://github.com/name/">GitHub</a>, <a href="https://www.linkedin.com/in/charliemaddex/">LinkedIn</a> <a href="mailto:charlie@maddex.dev">Email</a></small>
-    </div>
+<body class="animate__animated animate__fadeIn">
+    <header>
+        <h1>Charlie Maddex</h1>
+    </header>
+    <main id="maincontent">
+        <section>
+            <h2>About Me</h2>
+            <p>
+                <strong>Senior Technical Analyst at Method-IT. FinOps at Multiorb.</strong>
+            </p>
+            <p>
+                As a Senior Technical Analyst at Method-IT, my expertise lies in systems architecture and support, with a strong emphasis on fortifying security and ensuring platform resilience. My responsibilities encompass proactive monitoring, iterative development, systematic patching, and swift incident resolution. Parallel to this role, I contribute as a Backend Engineer at Multiorb, where I engineer solutions for automated cloud cost optimization across diverse cloud environments.
+            </p>
+            <p>
+                Connect with me on: <a href="https://x.com/cunjur/" rel="noopener">Twitter</a>, <a href="https://github.com/name/" rel="noopener">GitHub</a>, <a href="https://www.linkedin.com/in/charliemaddex/" rel="noopener">LinkedIn</a>, or <a href="mailto:charlie@maddex.dev">Email</a>.
+            </p>
+        </section>
+        <section>
+            <p>
+                <strong>Projects I have worked on:</strong>
+            </p>
+            <ul>
+                <li><a href="https://github.com/name/safe-transmission" rel="noopener">Safe Transmission</a> - Sending encrypted json data between client and server.</li>
+            </ul>
+        </section>
+    </main>
+    <footer>
+        <small>&copy; 2023 Charlie Maddex. All rights reserved.</small>
+    </footer>
 </body>
 </html>
