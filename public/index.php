dfa9031325c30a834a731b46707a0d69dfb483af
charlie <charlie@maddex.dev>
2023-11-28 00:38:24 +0000

    Update index.php with new styling and project links

--- public/index.php
+++ public/index.php
@@ -30,37 +30,48 @@
     <meta name="twitter:title" content="Charlie Maddex">
     <meta name="twitter:description" content="Charlie Maddex's Website">
     <meta name="twitter:image" content="https://maddex.dev/fav.jpg">
+
+    <style>
+        body {
+            line-height: 1.4;
+            font-size: 16px;
+            padding: 0 10px;
+            margin: 50px auto;
+            max-width: 650px;
+        }
+
+        ul {
+            margin-top: 0px;
+        }
+
+        #maincontent {
+            max-width: 38em;
+            margin: 15 auto;
+        }
+
+        h2 {
+            margin-bottom: 0px;
+        }
+    </style>
 </head>
 
 <body class="animate__animated animate__fadeIn">
-    <header>
-        <h1>Charlie Maddex</h1>
-    </header>
-    <main id="maincontent">
-        <section>
-            <p>
-                <strong>Senior Technical Analyst at Method-IT. Backend Engineer at Multiorb.</strong>
-            </p>
-            <p>
-                Senior Technical Analyst with a focus on systems integrity and innovation. I also contribute as a Backend Engineer at <a href="https://multiorb.net" rel="noopener">Multiorb</a>, where I engineer software systems for clients.
-                Currently on a side quest: building an ultra-compact home compute cluster.
-            </p>
-            <p>
-                Connect with me on: <a href="https://x.com/cunjur/" rel="noopener">Twitter</a>, <a href="https://github.com/name/" rel="noopener">GitHub</a>, <a href="https://www.linkedin.com/in/charliemaddex/" rel="noopener">LinkedIn</a>, or <a href="mailto:charlie@maddex.dev">Email</a>.
-            </p>
-        </section>
-        <section>
-            <p>
-                <strong>Projects I have worked on:</strong>
-            </p>
-            <ul>
-                <li><a href="https://github.com/name/safe-transmission" rel="noopener">Safe Transmission</a> - Sending encrypted json data between client and server.</li>
-                <li><a href="https://github.com/name/autopilot" rel="noopener">Autopilot</a> - Optimize your Microsoft Azure environment for cost-efficiency.</li>
-            </ul>
-        </section>
-    </main>
-    <footer>
-        <small>&copy; 2023 Charlie Maddex. All rights reserved.</small>
-    </footer>
+        <div id="maincontent" style="margin-top:70px">
+        <h2>Charlie Maddex</h2><br>
+        <p>Senior Techincal Analyst. Eng. @Multiorb</p>
+        <p>Currently on a side quest: building an ultra-compact home compute cluster.</p>
+        <p>
+            Connect with me on: <a href="https://x.com/cunjur/" rel="noopener">Twitter</a>, <a href="https://github.com/name/" rel="noopener">GitHub</a>, <a href="https://www.linkedin.com/in/charliemaddex/" rel="noopener">LinkedIn</a>, or <a href="mailto:charlie@maddex.dev">Email</a>.
+        </p>
+        <ul>
+            <li><a href="https://github.com/name/safe-transmission" rel="noopener">Safe Transmission</a>: Sending encrypted json data between client and server.</li>
+            <li><a href="https://github.com/name/autopilot" rel="noopener">Autopilot</a>: Optimize your Microsoft Azure environment for cost-efficiency.</li>
+            <li><a href="https://github.com/name/invite" rel="noopener">Invite</a>: Self-hosted waitlist & invite platform.</li>
+            <li><a href="https://github.com/name/install-handler" rel="noopener">Install Handler</a>: A simple install handler for Windows.</li>
+            <li><a href="https://github.com/sakuraji-labs/log4j-remediation" rel="noopener">Log4j</a>: Discover and remediate Log4Shell vulnerability [CVE-2021-45105].</li>
+            <li><a href="https://github.com/name/screenshot-verification" rel="noopener">Screenshot Verification</a>: Verify the absence of error messages after performing screenshot verification.</li>
+        </ul>
+        <small><sup>*</sup> built with <a href="https://twitter.com/danielgross/status/1668655427243081735" rel="noopener">nat.css</a></small>
+    </div>
 </body>
 </html>
